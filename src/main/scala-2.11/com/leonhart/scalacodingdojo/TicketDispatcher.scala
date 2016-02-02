a61e0dcc916f5b4b24bde324aaf8a3ec80a79640
David LEONHARTSBERGER <David LEONHARTSBERGER@frequentis.com>
2016-02-02 19:13:52 +0100

    Starting round 2

--- src/main/scala-2.11/com/leonhart/scalacodingdojo/TicketDispatcher.scala
+++ src/main/scala-2.11/com/leonhart/scalacodingdojo/TicketDispatcher.scala
@@ -1,17 +1,18 @@
 package com.leonhart.scalacodingdojo
 
-import com.leonhart.scalacodingdojo.Theater.{Row, SeatNumber}
+import com.leonhart.scalacodingdojo.Theater.{SeatNumber, Row}
 import com.leonhart.scalacodingdojo.TicketDispatcher.Reservation
 
 object TicketDispatcher {
   type Reservation = Set[Seat]
-
-  case class Seat(val row: Row, val seatNumber: SeatNumber)
 }
+
+case class Seat(val row: Row, val seatNumber: SeatNumber)
 
 class TicketDispatcher(val theater: Theater) {
 
-  def reserveSeat(): Option[Reservation] =
-    Some(Set())
+  def reserveSeat(): Reservation = {
+    Set(Seat('A', 8))
+  }
 
 }
--- src/test/scala-2.11/com/leonhart/scalacodingdojo/TheaterSpec.scala
+++ src/test/scala-2.11/com/leonhart/scalacodingdojo/TheaterSpec.scala
@@ -1,21 +1,36 @@
 package com.leonhart.scalacodingdojo
 
-import com.leonhart.scalacodingdojo.Theater.{SeatNumber, Row}
+import com.leonhart.scalacodingdojo.Theater.{SeatCount, Row}
 import org.scalatest.{FlatSpec, Matchers}
 
 class TheaterSpec extends FlatSpec with Matchers {
 
+  var rows = Map[Row, SeatCount]()
+  rows += ('A' -> 15)
+  rows += ('B' -> 15)
+  rows += ('C' -> 15)
+
+  val theater = new Theater(rows)
+  val dispatcher = new TicketDispatcher(theater)
+
   "book seat in empty theater" should "get new reservation" in {
-    var rows = Map[Row, SeatNumber]()
-    rows += ('A' -> 15)
-    rows += ('B' -> 15)
-    rows += ('C' -> 15)
-
-    val theater = new Theater(rows)
-
-    val dispatcher = new TicketDispatcher(theater)
     val reservedSeats = dispatcher.reserveSeat()
-
     reservedSeats should not be empty
+  }
+
+  "book seat in empty theater" should "get reservation with central seat" in {
+    val reservedSeats = dispatcher.reserveSeat()
+    reservedSeats shouldEqual Set(Seat('A', 8))
+  }
+
+  "book seat in not-empty theater" should "get reservation near the center" in {
+    // given
+    val reservedSeat = dispatcher.reserveSeat()
+
+    // when
+    val anotherReservedSeat = dispatcher.reserveSeat()
+
+    // then
+    anotherReservedSeat should not equal reservedSeat
   }
 }
