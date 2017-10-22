7f401df13cb252dc6cc66cc4c7e0be7af2956346
David Leonhart <leozilla@gmail.com>
2017-10-22 13:39:28 +0200

    Final solution

--- src/main/scala/Dummy.scala
+++ src/main/scala/Dummy.scala
@@ -4,6 +4,9 @@ object Dummy {
 
   def comp(seq1: Seq[Int], seq2: Seq[Int]): Boolean = {
     if (seq1 == null || seq2 == null) false
-    else false
+    else {
+      val doubled = seq1.map(i => i * i).sorted
+      seq2.sorted.zip(doubled).forall({ case (s1: Int, s2: Int) => s1 == s2 })
+    }
   }
 }
--- src/test/scala/DummySpec.scala
+++ src/test/scala/DummySpec.scala
@@ -9,4 +9,22 @@ class DummySpec extends FlatSpec with Matchers {
     val seq2 = Seq(11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19)
     Dummy.comp(seq1, seq2) should be (true)
   }
+
+  "not same sequences" should "fail" in {
+    val seq1 = Seq(121, 144, 12)
+    val seq2 = Seq(11*11, 121*121, 144*144)
+    Dummy.comp(seq1, seq2) should be (false)
+  }
+
+  "empty seq1" should "fail" in {
+    val seq1 = Seq()
+    val seq2 = Seq(11*11, 121*121, 144*144)
+    Dummy.comp(seq1, seq2) should be (false)
+  }
+
+  "number of occurences dont match" should "fail" in {
+    val seq1 = Seq(2, 2, 3)
+    val seq2 = Seq(4, 9, 9)
+    Dummy.comp(seq1, seq2) should be (false)
+  }
 }
