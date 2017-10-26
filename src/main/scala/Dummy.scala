9d40e29bd4036cf68d18276dbb3b616bfc256062
David Leonhart <leozilla@gmail.com>
2017-10-26 18:30:46 +0200

    Working version

--- src/main/scala/Dummy.scala
+++ src/main/scala/Dummy.scala
@@ -3,6 +3,15 @@ package com.leonhart.scalacodingdojo
 object Solution {
 
   def duplicateEncode(word: String) = {
-    // Your code here
+    val countOfChars = scala.collection.mutable.HashMap[Char, Int]()
+    val lower = word.toLowerCase
+    for (c <- lower) {
+        countOfChars.update(c, countOfChars.getOrElse(c, 0) + 1)
+    }
+
+    lower.map({
+      case v if countOfChars(v) <= 1 => "("
+      case _ => ")"
+    }).reduce(_ + _)
   }
 }
