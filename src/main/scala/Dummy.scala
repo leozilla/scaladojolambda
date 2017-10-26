7b6f54e17b288da84bbdbeeea24285d7349de720
David Leonhart <leozilla@gmail.com>
2017-10-26 19:41:57 +0200

    First draft

--- src/main/scala/Dummy.scala
+++ src/main/scala/Dummy.scala
@@ -1,5 +1,30 @@
 package com.leonhart.scalacodingdojo
 
-object Dummy {
+import scala.collection.mutable.ListBuffer
 
+object Lcs {
+  def lcs(a: String, b: String): String = {
+    var subSequences = new ListBuffer[String]()
+
+    var aIndex = 0
+    var bIndex = 0
+
+    var possibleSequence = new ListBuffer[Char]()
+
+    while (aIndex < a.length && bIndex < b.length) {
+      val aChar = a(aIndex)
+      val bChar = b(bIndex)
+      if (aChar == bChar) { // char does match
+        possibleSequence += aChar
+        aIndex += 1
+        bIndex += 1
+      } else {
+        aIndex += 1
+      }
+    }
+
+    subSequences += possibleSequence.mkString
+
+    subSequences.sortBy(_.length).headOption.getOrElse("")
+  }
 }
--- src/test/scala/DummySpec.scala
+++ src/test/scala/DummySpec.scala
@@ -1,9 +1,36 @@
 package com.leonhart.scalacodingdojo
 
-import org.scalatest.{Matchers, FlatSpec}
+import org.junit.Assert._
+import org.junit.Test
 
-class DummySpec extends FlatSpec with Matchers {
+class LcsTest {
+  @Test def a(): Unit = {
+    assertEquals("", Lcs.lcs("", ""))
+    assertEquals("", Lcs.lcs("abc", ""))
+    assertEquals("", Lcs.lcs("", "abc"))
+    assertEquals("", Lcs.lcs("a", "b"))
+    assertEquals("a", Lcs.lcs("a", "a"))
+    assertEquals("abc", Lcs.lcs("abcdef", "abc"))
+  }
 
-  "some test" should "not fail" in {
+  @Test def b(): Unit = {
+    assertEquals("ac", Lcs.lcs("abc", "ac"))
+    assertEquals("acf", Lcs.lcs("abcdef", "acf"))
+  }
+
+  @Test def c(): Unit = {
+    assertEquals("nottest", Lcs.lcs("anothertest", "notatest"))
+  }
+
+  @Test def d(): Unit = {
+    assertEquals("12356", Lcs.lcs("132535365", "123456789"))
+  }
+
+  @Test def e(): Unit = {
+    assertEquals("final", Lcs.lcs("nothardlythefinaltest", "zzzfinallyzzz"))
+  }
+
+  @Test def f(): Unit = {
+     assertEquals("acdefghijklmnoq", Lcs.lcs("abcdefghijklmnopq", "apcdefghijklmnobq"))
   }
 }
